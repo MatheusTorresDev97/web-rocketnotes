5290873b4eeea63df14ca958f940eeb1c2792ad7
Matheus <matheustorresdev@gmail.com>
2023-11-15 19:10:20 -0300

    imagem do usuario

--- front-rocketnotes/src/components/Header/index.jsx
+++ front-rocketnotes/src/components/Header/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 
 
 const Header = () => {
   return (
     <Container>
-      
+      <Profile>
+      <img
+          src="https://github.com/matheustorresdev97.png"
+          alt="Foto do usuário"
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Matheus Torres</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
