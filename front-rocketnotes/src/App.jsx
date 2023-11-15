8a9fcd0baf1026360b8e786c61fec4fe5ed5618e
Matheus <matheustorresdev@gmail.com>
2023-11-15 19:46:09 -0300

    Aplicando Grid

--- front-rocketnotes/src/App.jsx
+++ front-rocketnotes/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
-import Details from './pages/Details'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   )
 }
---
+++ front-rocketnotes/src/pages/Home/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+import Header from '../../components/Header'
+
+const Home = () => {
+  return (
+    <Container>
+    <Brand>
+    <h1>Rocketnotes</h1>
+    </Brand>
+
+    <Header></Header>
+
+    <Menu>
+
+    </Menu>
+
+    <Search>
+
+    </Search>
+
+    <Content>
+
+    </Content>
+
+    <NewNote>
+        
+    </NewNote>
+    </Container>
+  )
+}
+
+export default Home
