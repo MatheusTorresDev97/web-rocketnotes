6d79b42cbecd4802fb9603d2bbdd36838660f1b6
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-16 09:41:06 -0300

    Header da New

--- front-rocketnotes/src/pages/New/index.jsx
+++ front-rocketnotes/src/pages/New/index.jsx
@@ -1,10 +1,23 @@
 import Header from '../../components/Header'
-import { Container } from './styles'
+import Input from '../../components/Input'
+
+import { Container, Form } from './styles'
 
 const New = () => {
   return (
     <Container>
         <Header />
+
+        <main>
+          <Form>
+            <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+            </header>
+
+            <Input placeholder="Título" />
+          </Form>
+        </main>
     </Container>
   )
 }
