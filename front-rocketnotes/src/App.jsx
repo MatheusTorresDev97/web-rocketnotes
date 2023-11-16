989de9e4d2d03add48e30d50fb5867a1ddc3b225
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-16 08:53:41 -0300

    criando a pagina de login

--- front-rocketnotes/src/App.jsx
+++ front-rocketnotes/src/App.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
-import Home from './pages/Home'
+import SignIn from './pages/SignIn'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   )
 }
