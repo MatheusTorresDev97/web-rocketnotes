cc787ab87f1fb8e1697213c49b12fea1acb1ed43
Matheus <matheustorresdev@gmail.com>
2023-11-19 13:08:16 -0300

    Redirecionando e Protegendo Rotas

--- front-rocketnotes/src/routes/index.jsx
+++ front-rocketnotes/src/routes/index.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 import AuthRoutes from "./AuthRoutes";
+import AppRoutes from "./AppRoutes"
 
 const Routes = () => {
+  const { user } = useAuth()
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 };
