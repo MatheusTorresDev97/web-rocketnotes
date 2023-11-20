1042c115662ea00b0b74d2db7addb4c80b5e496d
Matheus <matheustorresdev@gmail.com>
2023-11-19 21:01:45 -0300

    Remover Links

--- front-rocketnotes/src/pages/New/index.jsx
+++ front-rocketnotes/src/pages/New/index.jsx
@@ -5,37 +5,63 @@ import Textarea from '../../components/Textarea'
 import Section from '../../components/Section'
 import NoteItem from '../../components/NoteItem'
 import { Container, Form } from './styles'
+import { useState } from 'react'
 
 
 const New = () => {
+  const [links, setLinks] = useState([])
+  const [newLink, setNewLink] = useState()
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink])
+    setNewLink("")
+  }
+
+  function handleRemoveLink(deleted) {
+    setLinks(prevState => prevState.filter((link) => link !== deleted))
+  }
+
   return (
     <Container>
-        <Header />
+      <Header />
 
-        <main>
-          <Form>
-            <header>
+      <main>
+        <Form>
+          <header>
             <h1>Criar nota</h1>
             <Link to="/">voltar</Link>
-            </header>
+          </header>
 
-            <Input placeholder="Título" />
+          <Input placeholder="Título" />
 
-            <Textarea placeholder="Observações" />
+          <Textarea placeholder="Observações" />
 
-            <Section title="Links úteis">
-                <NoteItem value='https://google.com.br' />
-                <NoteItem isNew placeholder="Novo link" />
-            </Section>
+          <Section title="Links úteis">
+            {
+              links.map((link, index) =>(
+                <NoteItem
+                key={String(index)}
+                value={link}
+                onClick={() => {handleRemoveLink(link)}}
+                />
+              ))
+            }
+            <NoteItem
+              isNew
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+              placeholder="Novo link" />
+          </Section>
 
-            <Section title="Marcadores">
-                <div className="tags">
-                  <NoteItem value="react" />
-                  <NoteItem isNew placeholder="Nova tag" />
-                </div>
-            </Section>
-          </Form>
-        </main>
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+        </Form>
+      </main>
     </Container>
   )
 }
